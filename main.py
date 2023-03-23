4c6a3ba08b01e50aabb1cbbe786a6fded56ec747
Jltello <jltello96@gmail.com>
2023-03-22 22:47:27 -0600

    adds

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+import openai
+import os
+
+app = FastAPI()
+
+load_dotenv()
+
+openai.api_key = os.getenv('SECRET_KEY')
+
+class Prompt(BaseModel):
+    text: str = Field(min_length=10, max_length=100)
+
+@app.post('/chat')
+def generate_response(prompt: Prompt):
+    text_length = 1000
+    gpt3_model = "davinci"
+    response = openai.Completion.create(
+        engine=gpt3_model,
+        prompt=prompt.text,
+        max_tokens = 3,
+        n=1,
+        stop=None,
+        temperature=0.5
+    )
+    return response['choices'][0]['text']
